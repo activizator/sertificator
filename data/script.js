8a77943ffeedfb5b86089c690cba007e59eab51d
Activizator <activizator@ya.ru>
2021-12-26 18:52:16 +0300

    unlimited fio list

--- data/script.js
+++ data/script.js
@@ -1,10 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", fio);
+
+function fio() {
   let fio = decodeURI(window.location.hash);
   if (fio === "" || fio === "#") return;
   document.title = fio;
   fio = fioBR(fio);
   document.getElementById("fio").innerHTML = fio;
-});
+  document.removeEventListener("DOMContentLoaded", fio);
+}
 
 function fioBR(fio) {
   return fio.replace("#", "").replace(" ", "<br>");
--- src/index.js
+++ src/index.js
@@ -13,9 +13,18 @@ async function printPDF(name) {
     path: "./result/" + name + ".pdf",
     printBackground: true,
   });
-
+  await page.close();
   await browser.close();
   return pdf;
 }
 
-list.forEach((fio) => printPDF(fio));
+let offset = 0;
+for (let i = 0; i < list.length; i++) {
+  setTimeout(() => {
+    (async () => {
+      await printPDF(list[i]);
+      console.log(i + 1 + ". Готово: " + list[i]);
+    })();
+  }, 10000 + offset);
+  offset += 10000;
+}
