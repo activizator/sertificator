927f9669890e2e85a4e978329875fe84f780a5aa
Activizator <activizator@ya.ru>
2021-12-31 12:14:36 +0300

    List sort and remove doubles #1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,31 @@ const puppeteer = require("puppeteer");
 const path = require("path");
 const list = require("../data/list.js");
 
+let myList = list.map(el => el.replace(/\s+/g, ' ').trim());
+myList = [...new Set(myList)]
+  .sort((a, b) => a.localeCompare(b))
+  .map((el) => {
+    return el
+      .split(" ")
+      .map((x) => capitalizeFirstLetter(x.trim()))
+      .join(" ");
+  });
+
+let offset = 0;
+for (let i = 0; i < myList.length; i++) {
+  setTimeout(() => {
+    (async () => {
+      await printPDF(myList[i]);
+      console.log(i + 1 + ". Готово: " + myList[i]);
+    })();
+  }, 10000 + offset);
+  offset += 10000;
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
 async function printPDF(name) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
@@ -16,15 +41,4 @@ async function printPDF(name) {
   await page.close();
   await browser.close();
   return pdf;
-}
-
-let offset = 0;
-for (let i = 0; i < list.length; i++) {
-  setTimeout(() => {
-    (async () => {
-      await printPDF(list[i]);
-      console.log(i + 1 + ". Готово: " + list[i]);
-    })();
-  }, 10000 + offset);
-  offset += 10000;
 }
